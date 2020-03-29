5af088863855e808536c25bf738f2597955b3f4b
WenRong <hybrid0930@gmail.com>
2020-03-29 23:06:32 +0800

    [fix] android keystroe asset setting bug

--- Assets/JenkinsBuild/AndroidKeyStore.cs
+++ Assets/JenkinsBuild/AndroidKeyStore.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "AndroidKeyStore", menuName = "JenkinsBuild/Creat AndroidKeyStore", order = 1)]
 public class AndroidKeyStore : ScriptableObject
@@ -9,5 +7,4 @@ public class AndroidKeyStore : ScriptableObject
     public string KeystorePassword;
     public string AliasName;
     public string AliasPassword;
-    public string[] Levels;
 }
--- Assets/JenkinsBuild/Editor/AndroidKeyStoreEditor.cs
+++ Assets/JenkinsBuild/Editor/AndroidKeyStoreEditor.cs
@@ -1,47 +1,74 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.IO;
 using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(AndroidKeyStore))]
 public class AndroidKeyStoreEditor : Editor
 {
-    float labelWidth = 125f;
+    private float labelWidth = 125f;
+    private AndroidKeyStore m_Target;
 
-    AndroidKeyStore m_Target;
+    private void OnEnable()
+    {
+        m_Target = (AndroidKeyStore)target;
+    }
 
     public override void OnInspectorGUI()
     {
-        m_Target = (AndroidKeyStore)target;
+        SetKeyStorePath();
+        SetKeyStorePassword();
+        SetAliasName();
+        SetAliasPassword();
+        SaveAsset();
+    }
 
+    private void SaveAsset()
+    {
         GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Select keystore", GUILayout.Width(150f)))
+        if (GUILayout.Button("Save"))
         {
-            string path = EditorUtility.OpenFilePanel("Select Keystore", Application.streamingAssetsPath, "keystore");
-            if (path.Length != 0)
-                m_Target.KeystoreName = path;
+            EditorUtility.SetDirty(m_Target);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
         }
+        GUILayout.EndHorizontal();
+    }
+
+    private void SetAliasPassword()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Alias Password:", GUILayout.Width(labelWidth));
+        m_Target.AliasPassword = GUILayout.TextField(m_Target.AliasPassword);
+        GUILayout.EndHorizontal();
+    }
+
+    private void SetAliasName()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Alias Name:", GUILayout.Width(labelWidth));
+        m_Target.AliasName = GUILayout.TextField(m_Target.AliasName);
+        GUILayout.EndHorizontal();
+    }
+
+    private void SetKeyStorePassword()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Keystore Password:", GUILayout.Width(labelWidth));
+        m_Target.KeystorePassword = GUILayout.TextField(m_Target.KeystorePassword);
+        GUILayout.EndHorizontal();
+    }
+
+    private void SetKeyStorePath()
+    {
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Select keystore", GUILayout.Width(labelWidth)))
+            m_Target.KeystoreName = EditorUtility.OpenFilePanel("Select Keystore", Application.streamingAssetsPath, "keystore");
+
         GUILayout.EndHorizontal();
 
         GUILayout.BeginHorizontal();
         GUILayout.Label("Path:", GUILayout.Width(labelWidth));
         GUILayout.Label(m_Target.KeystoreName);
         GUILayout.EndHorizontal();
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Keystore Password:", GUILayout.Width(labelWidth));
-        m_Target.KeystorePassword = GUILayout.TextField(m_Target.KeystorePassword);
-        GUILayout.EndHorizontal();
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Alias Name:", GUILayout.Width(labelWidth));
-        m_Target.AliasName = GUILayout.TextField(m_Target.AliasName);
-        GUILayout.EndHorizontal();
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Alias Password:", GUILayout.Width(labelWidth));
-        m_Target.AliasPassword = GUILayout.TextField(m_Target.AliasPassword);
-        GUILayout.EndHorizontal();
-
     }
 }
